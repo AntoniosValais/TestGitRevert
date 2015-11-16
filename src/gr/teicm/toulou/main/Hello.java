38281afcd6c0614c48e690e33c638360e496bbc9
AntoniosValais <toniovkozz@gmail.com>
2015-11-16 13:40:39 +0200

    Initial Commit
    Signed-off-by: AntoniosValais <toniovkozz@gmail.com>

---
+++ src/gr/teicm/toulou/main/Hello.java
@@ -0,0 +1,18 @@
+package gr.teicm.toulou.main;
+
+/*
+*
+* @Author AntoniosValais
+*
+*/
+
+public class Hello
+{
+
+	public static void main( String[] args )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
